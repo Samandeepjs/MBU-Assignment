0c7bbf46e8f6abf333a50a75bf3985205254f194
Samandeepjs <samandhindsa11@gmail.com>
2020-08-23 20:24:18 -0400

    final

--- server/client/src/components/auth/Logout.js
+++ server/client/src/components/auth/Logout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import { Link } from 'react-router-dom';
 
 class Logout extends Component {
   componentDidMount() {
